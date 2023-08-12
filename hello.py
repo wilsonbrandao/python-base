a0c41180d7d3a5eae820efb8436a1426ba44d921
wilsonbrandao <wilsonbrandaocosta@gmail.com>
2023-08-12 20:06:29 -0300

    hello multilinguas

--- hello.py
+++ hello.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python3
 
 #comentário de uma linha
-'''
-Comentário 
+""""Comentário 
 de 
 multiplas 
 linhas
-'''
-print("Hello World!".upper())
+"""
+__version__ = "0.0.1"
+__author__ = "Bruno Rocha"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "de_DE":
+    msg = "Hallo, Welt!"
+
+print(msg.upper())
+print(current_language)
